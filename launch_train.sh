4872d1771a68a645990f986ea068248113142fab
Liam <liamaltarac@gmail.com>
2025-07-10 15:34:24 -0400

    .

--- launch_train.sh
+++ launch_train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /env/bin/activate
-python ./train_no_hvd.py --model resnet18 --init he_normal --epochs 90  --ckpt_dir ckpts  --batch 32
+python ./train.py --model resnet18 --init he_normal --epochs 90  --ckpt_dir ckpts  --batch 32
